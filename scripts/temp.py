85d59a594837450563f4ff79857e30f6906eb984
Michael Poirier <mikepoirier26@gmail.com>
2016-09-15 18:33:11 -0600

    Add temp script. Add networkmanager-dmenu

--- scripts/temp.py
+++ scripts/temp.py
@@ -2,4 +2,7 @@ file = open("/sys/class/thermal/thermal_zone0/temp", "r")
 
 temp = float(file.read())
 
-print("{0:.0f}°C".format(temp / 1000))
+celsius = temp / 1000
+fahrenheit = celsius * (9/5) + 32
+
+print("{0:.0f}°C({1:.0f}°F)".format(celsius, fahrenheit))
